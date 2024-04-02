216a9dfefd63e8d720b0629ca15f42443e208bc9
alexandradenk <xendi3008@gmail.com>
2024-04-02 10:28:34 +0200

    Add classes for database

--- backend/myapi/models.py
+++ backend/myapi/models.py
@@ -1,3 +1,38 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Point(models.Model):
+    pID = models.IntegerField(primary_key=True)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    quadrant = models.IntegerField()
+
+    def __str__(self):
+        return f"({self.x}|{self.y})"
+
+
+class Patient(models.Model):
+    ssNr = models.IntegerField(primary_key=True, validators=[
+        RegexValidator(r'^\d{10}$')])
+    lastName = models.CharField()
+    firstName = models.CharField()
+
+    def __str__(self):
+        return f"{self.lastName} {self.firstName}"
+
+
+class Examination(models.Model):
+    exID = models.IntegerField(primary_key=True)
+    date = models.DateField()
+    result = models.ManyToOneRel
+
+
+class Result(models.Model):
+    resID = models.IntegerField(primary_key=True)
+    seen = models.BooleanField()
+
+
+
+
+
