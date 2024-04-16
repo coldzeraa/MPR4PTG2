1c4841aa9a5347a06a1e357a6a4150eb08ea379f
tini <mart1na.pl3tz@gmail.com>
2024-04-16 11:54:27 +0200

    Add Welcome Page and Connect to other Pages

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -2,54 +2,58 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-function Login(){
- 
+function Login() {
+
+  // Handle Navigation To Hello World Page
   const navigate = useNavigate();
+  const navigateToHelloWorld = () => {
+    navigate("/hello_world");
+  };
 
-    const navigate_to_hello_world = () => {
-     navigate("/hello_world");
-    };
+  return (
+    // Formatting
+    <div className="welcome-screen-container">
+      <div className="welcome-screen-background"></div>
+      <div className="content">
 
-    return (
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-          <div>
-            <h2>Persönliche Daten</h2>
-            <form>
-              <div style={{ marginBottom: "10px" }}>
-                <label htmlFor="firstName" style={{ display: "block" }}/>
-                <input
-                  type="text"
-                  id="firstName"
-                  name="firstName"
-                  placeholder="Vorname"
-                />
-              </div>
-              <div style={{ marginBottom: "10px" }}>
-                <label htmlFor="lastName" style={{ display: "block" }}/>
-                <input
-                  type="text"
-                  id="lastName"
-                  name="lastName"
-                  placeholder="Nachname"
-                />
-              </div>
-              <div style={{ marginBottom: "10px" }}>
-                <label htmlFor="email" style={{ display: "block" }}/>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Email"
-                />
-              </div>
-              <button type="submit" onClick={navigate_to_hello_world}>Weiter</button>
-              <button type="submit">Überspringen</button>
-            </form>
+        {/*Input Form*/}
+        <h2>Persönliche Daten</h2>
+        <form>
+          <div style={{ marginBottom: "10px" }}>
+            <label htmlFor="firstName" style={{ display: "block" }} />
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              placeholder="Vorname"
+            />
           </div>
-        </div>
-      );
-      
+          <div style={{ marginBottom: "10px" }}>
+            <label htmlFor="lastName" style={{ display: "block" }} />
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              placeholder="Nachname"
+            />
+          </div>
+          <div style={{ marginBottom: "10px" }}>
+            <label htmlFor="email" style={{ display: "block" }} />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Email"
+            />
+          </div>
 
+          {/*Submit Button and Skip Button*/}
+          <button type="submit" onClick={navigateToHelloWorld}>Weiter</button>
+          <button type="submit">Überspringen</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
