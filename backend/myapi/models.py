6b03c9e037a731e30bc2bb602e4642a90d029ea1
alexandradenk <xendi3008@gmail.com>
2024-04-22 13:57:34 +0200

    Altered models

--- backend/myapi/models.py
+++ backend/myapi/models.py
@@ -13,7 +13,7 @@ class Point(models.Model):
 
 
 class Patient(models.Model):
-    patID = models.IntegerField(primary_key=True)
+    patID = models.AutoField(primary_key=True)
     lastName = models.CharField(max_length=50)       # last name
     firstName = models.CharField(max_length=50)      # first name
 
@@ -22,15 +22,15 @@ class Patient(models.Model):
 
 
 class Examination(models.Model):
-    exID = models.IntegerField(primary_key=True)    # ID
+    exID = models.AutoField(primary_key=True)    # ID
     date = models.DateField()                       # date of examination
 
-    result = models.ForeignKey(Patient, on_delete=models.CASCADE)   # patient
+    patientID = models.ForeignKey(Patient, on_delete=models.CASCADE)   # patient
 
 
 class PointResult(models.Model):
-    resID = models.IntegerField(primary_key=True)   # ID
+    resID = models.AutoField(primary_key=True)   # ID
     seen = models.BooleanField()                    # point seen (t/f)
 
-    point = models.ForeignKey(Point, on_delete=models.CASCADE)              # point
-    examination = models.ForeignKey(Examination, on_delete=models.CASCADE)  # examination
+    pointID = models.ForeignKey(Point, on_delete=models.CASCADE)              # point
+    examinationID = models.ForeignKey(Examination, on_delete=models.CASCADE)  # examination
