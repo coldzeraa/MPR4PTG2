254287e448e01a01ef12c5317afa3ea34850d5da
tini <mart1na.pl3tz@gmail.com>
2024-06-07 09:21:15 +0200

    Change Infotags

--- frontend/src/components/tutorial.js
+++ frontend/src/components/tutorial.js
@@ -51,14 +51,12 @@ function Tutorial() {
             <div className="content">
                 <h1>Information</h1>
                 <div className='scroll-box border rounded p-3 content-left'>
-                <p>
                     Beachten Sie bitte folgende Hinweise, um OptiMate optimal nutzen zu können:<br/><br/>
                     <ol>
                         <li><b>Wählen Sie eine ruhige Umgebung für die Untersuchung.</b><br/>Vermeiden Sie Störgeräusche, um das Ergebnis nicht zu verfälschen.</li><br/>
                         <li><b>Starten Sie erst, wenn Sie bereit sind.</b><br/>Durch den Druck auf "Start" wird ein Countdown gestartet, der Ihnen 15 Sekunden Zeit gibt, um Ihr Mobiltelefon in die VR-Vorrichtung einzusetzen.</li><br/>
                         <li><b>Handeln Sie in Ihrem eigenen Interesse.</b><br/>Um ein möglichst aussagekräftiges Ergebnis zu erhalten, sollten Sie nur dann angeben, einen Punkt gesehen zu haben, wenn Sie ihn tatsächlich wahrgenommen haben.</li>
                     </ol>
-                </p>
                 </div>
                 {/*Login Button*/}
                 <br/>
