53e8e1c1fe7d1d92551cde10b3fa2b44a8be9c70
Nico Pointner <Nico Pointner@DESKTOP-0FSC8JG>
2024-06-18 13:16:59 +0200

    Add creating PDF with name and some details

--- backend/myapi/urls.py
+++ backend/myapi/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('perimetry/', views.perimetry, name='perimetry'),
     path('get_points/', views.get_points, name='get_points'),
     path('emailing/', views.emailing, name='emailing'),
-    path('examination/', views.examination, name='examination')
+    path('examination/', views.examination, name='examination'),
+    path('get_pdf/', views.get_pdf, name='get_pdf')
 ]
