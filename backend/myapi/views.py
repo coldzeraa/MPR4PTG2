0df1f9472929e889fc2c3a99d83d98cf34b76492
Nico Pointner <Nico Pointner@DESKTOP-0FSC8JG>
2024-06-18 13:16:59 +0200

    Add creating PDF with name and some details

--- backend/myapi/views.py
+++ backend/myapi/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -15,6 +16,13 @@ from myapi.model.Patient import Patient
 import datetime
 from myapi.point_administrator.PointAdministrator import PointAdministrator
 from myapi.export.email import send_email
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+import io
+from reportlab.lib.utils import ImageReader
+import os
+from backend.settings import BASE_DIR
+
 
 @api_view(['POST'])
 def login(request):
@@ -70,7 +78,6 @@ def emailing(request):
     else:
         return JsonResponse({'error': 'Invalid request method'}, status=405)
 
-
 @api_view(['POST'])
 def perimetry(request):
     if request.method == 'POST':
@@ -115,3 +122,99 @@ def examination(request):
         pat = PatientService.get(patID)
         ex = ExaminationService.store(pat, datetime.datetime.today())
     return JsonResponse({'exID': ex.exID}, status=200)
+
+@api_view(['GET'])
+def get_pdf(request):
+    if request.method == 'GET':
+        
+        # Extract examinationID from URL Parameter
+        exID = request.GET.get('id', '')
+        
+        # Get examination
+        examination = ExaminationService.get(exID)
+        
+        # Get patient
+        patient = PatientService.get(examination.pat.patID)
+        
+        buffer = io.BytesIO()
+        
+        # Create pdf
+        p = canvas.Canvas(buffer, pagesize=letter)
+        
+        # Title
+        p.setTitle("Optimate Testergebnis")
+        
+        # Headline
+        text = "OptiMate- Testergebnis"
+        text_width = p.stringWidth(text, "Helvetica-Bold", 16)
+        x = (letter[0] - text_width) / 2
+        p.setFont("Helvetica-Bold", 16)
+        p.drawString(x, 670, text)
+        
+        # Image
+        image_path = os.path.join(BASE_DIR, 'myapi', 'images', 'eye.png')
+        img = ImageReader(image_path)
+        p.drawImage(img, 50, 710, width=100, height=50)
+        
+        # Date
+        p.setFont('Helvetica', 10)
+        p.drawString(500, 740, str(examination.date))
+        
+        # Patient information
+        p.setFont("Helvetica", 12)
+        p.drawString(70, 620, f"Patient: {patient.firstName} {patient.lastName}")
+        
+        # Results
+        p.setFont("Helvetica", 11)
+        p.drawString(70, 550, "Testergebnis: ")
+        
+        # Testresult
+        image_path = os.path.join(BASE_DIR, 'myapi', 'images', 'david.png')
+        img = ImageReader(image_path)
+        p.drawImage(img, 110, 200, width=400, height=300)
+        
+        # Hint
+        p.setFont("Helvetica-Bold", 8)
+        p.drawString(70, 65, "[Wichtiger Hinweis]")
+
+        # Text of hint
+        p.setFont("Helvetica-Oblique", 8)
+        text = """Bitte beachten Sie, dass OptiMate kein zertifiziertes Medizinprodukt ist und keinesfalls eine professionelle 
+                    medizinische Diagnostik oder Therapie ersetzt. Konsultieren Sie bei medizinischen Anliegen stets einen qualifizierten 
+                    Facharzt."""
+                    
+        
+        # Position of text
+        x = 70
+        y = 50
+        width = 500
+        
+        # Split information text
+        lines = []
+        words = text.split()
+        line = ""
+        for word in words:
+            if p.stringWidth(line + " " + word) <= width:
+                line += " " + word if line else word
+            else:
+                lines.append(line.lstrip())
+                line = word
+        lines.append(line.lstrip())
+        
+        # Print every line
+        for line in lines:
+            p.drawString(x, y, line)
+            y -= 14
+        
+        p.showPage()
+        
+        p.save()
+        
+        # Set curser to beginning
+        buffer.seek(0)
+        
+        # Http response
+        response = HttpResponse(buffer, content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="medical_report.pdf"'
+        
+        return response
