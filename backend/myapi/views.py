ab8f957d475bed3c35378cf63cc81e1b2f6d9e72
Nico Pointner <Nico Pointner@DESKTOP-0FSC8JG>
2024-06-21 12:16:24 +0200

    Add delete <p> in info.js

--- backend/myapi/views.py
+++ backend/myapi/views.py
@@ -1,5 +1,4 @@
 from django.http import JsonResponse
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.decorators import api_view
 from myapi.db.PointResultService import PointResultService
