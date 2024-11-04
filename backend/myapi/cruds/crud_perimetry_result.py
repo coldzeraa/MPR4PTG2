ca4012d474e36bf6d2e1cd4aab3a6eef1230b891
Nico Pointner <Nico Pointner@DESKTOP-0FSC8JG>
2024-11-04 15:45:01 +0100

    Add updated database

--- backend/myapi/cruds/crud_perimetry_result.py
+++ backend/myapi/cruds/crud_perimetry_result.py
@@ -1,11 +1,11 @@
 from django.db.models import AutoField
 
-from myapi.models import PointResult
+from myapi.models import ResultPerimetry
 from myapi.model.Point import Point
 from myapi.model.Examination import Examination
 
 
-def create_point_result(seen: bool, p: Point, ex: Examination):
+def create_perimetry_result(seen: bool, p: Point, ex: Examination):
     """
         Create a new PointResult Object
 
@@ -15,27 +15,27 @@ def create_point_result(seen: bool, p: Point, ex: Examination):
         :return: new PointResult
        """
        
-    return PointResult.objects.create(seen=seen, p=p, ex=ex)
+    return ResultPerimetry.objects.create(seen=seen, p=p, ex=ex)
 
 
-def get_all_point_results():
+def get_all_perimetry_results():
     """
        Get all PointResults
     """
-    return PointResult.objects.all()
+    return ResultPerimetry.objects.all()
 
 
-def get_point_result_by_id(resID: AutoField):
+def get_perimetry_result_by_id(resID: AutoField):
     """
        Get PointResult by given ID
 
        :param resID: point result ID
        :return: PointResult Object
     """
-    return PointResult.objects.get(resID=resID)
+    return ResultPerimetry.objects.get(resID=resID)
 
 
-def update_point_result(resID: AutoField, seen: bool, pID: AutoField,  exID: AutoField):
+def update_perimetry_result(resID: AutoField, seen: bool, pID: AutoField,  exID: AutoField):
     """
           Update PointResult
 
@@ -43,7 +43,7 @@ def update_point_result(resID: AutoField, seen: bool, pID: AutoField,  exID: Aut
           :param seen: point seen by patient (true|false)
           :return: PatientResult Object
        """
-    point_result = PointResult.objects.get(resID=resID)
+    point_result = ResultPerimetry.objects.get(resID=resID)
     point_result.seen = seen
     point_result.ex = exID
     point_result.p = pID
@@ -51,24 +51,24 @@ def update_point_result(resID: AutoField, seen: bool, pID: AutoField,  exID: Aut
     return point_result
 
 
-def delete_point_result(resID: AutoField):
+def delete_perimetry_result(resID: AutoField):
     """
           Delete PointResult
 
           :param resID: id of point result
        """
-    point_result = PointResult.objects.get(resID=resID)
+    point_result = ResultPerimetry.objects.get(resID=resID)
     point_result.delete()
     
     
-def get_point_result_by_exID(exID: AutoField):
+def get_perimetry_result_by_exID(exID: AutoField):
     """
         Returns PointResults of given exID
 
         :param exID: id of examination
     """
     
-    return PointResult.objects.filter(ex = exID)
+    return ResultPerimetry.objects.filter(ex = exID)
     
     
     
