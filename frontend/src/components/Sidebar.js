0f095439cc37c0606d5b3aaf211b14a4f506b0b5
alexandradenk <xendi3008@gmail.com>
2024-11-11 15:55:51 +0100

    implemented base logic for sidebar disappear on "skip"

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,15 +1,8 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import '../App.css';
 import {IconMap} from '../data/IconMap';
 
-const menuItems = [
-    { icon: 'fas fa-tachometer-alt', label: 'Dashboard'},
-    { icon: 'fas fa-eye', label: 'Perimetrie'},
-    { icon: 'fas fa-eye-dropper', label: 'Ishihara-Test'},
-    { icon: 'fas fa-archive', label: 'Archiv'},
-];
-
-const MenuItem = ({ icon, label, expanded, link }) => (
+const MenuItem = ({icon, label, expanded, link}) => (
     <div
         className="menu-item"
         onClick={() => window.location.href = link}
@@ -23,22 +16,24 @@ const MenuItem = ({ icon, label, expanded, link }) => (
 
 export default function Sidebar() {
     const [expanded, setExpanded] = useState(false);
+     const isPatientLoggedIn = Boolean(localStorage.getItem('email'));
 
     const toggleSidebar = () => {
         setExpanded(prev => !prev);
     };
 
     return (
-        <div>
-            <button onClick={toggleSidebar} className="toggle-button">
-                <i className={`fas ${expanded ? 'fa-chevron-right' : 'fa-bars'}`} style={{ fontSize: '24px' }}></i>
-            </button>
+        isPatientLoggedIn && (
+            <div>
+                <button onClick={toggleSidebar} className="toggle-button">
+                    <i className={`fas ${expanded ? 'fa-chevron-right' : 'fa-bars'}`} style={{fontSize: '24px'}}></i>
+                </button>
 
-            <div className={`sidebar ${expanded ? 'expanded' : 'collapsed'}`}>
-                {expanded && (
-                    <>
-                        <h3 className="menu-title">Menü</h3>
-                        <div className="menu-items">
+                <div className={`sidebar ${expanded ? 'expanded' : 'collapsed'}`}>
+                    {expanded && (
+                        <>
+                            <h3 className="menu-title">Menü</h3>
+                            <div className="menu-items">
 
                                 {Object.entries(IconMap).map(([key, item], index) => (
                                     <MenuItem
@@ -49,32 +44,34 @@ export default function Sidebar() {
                                         link={item.link}
                                     />
                                 ))}
-                          
-                        </div>
-                        <div className="small-links">
-                            <a href="#" className="small-link">Über Optimate</a>
-                            <a href="#" className="small-link">Kontakt</a>
-                            <a href="#" className="small-link">Ausloggen</a>
-                        </div>
-                        <div className="flex justify-center items-center">
-                            <a
-                                className="flex items-center gap-1 text-sm hover:text-blue-700 hover:underline"
-                                href="https://github.com/coldzeraa/MPR4PTG2"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                Find us on{' '}
-                                <img
-                                    src={require('../icons/github.svg').default}
-                                    alt="GitHub Logo"
-                                    width={15}
-                                    height={15}
-                                />
-                            </a>
-                        </div>
-                    </>
-                )}
+
+                            </div>
+                            <div className="small-links">
+                                <a href="#" className="small-link">Über Optimate</a>
+                                <a href="#" className="small-link">Kontakt</a>
+                                <a href="#" className="small-link">Ausloggen</a>
+                            </div>
+                            <div className="flex justify-center items-center">
+                                <a
+                                    className="flex items-center gap-1 text-sm hover:text-blue-700 hover:underline"
+                                    href="https://github.com/coldzeraa/MPR4PTG2"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    Find us on{' '}
+                                    <img
+                                        src={require('../icons/github.svg').default}
+                                        alt="GitHub Logo"
+                                        width={15}
+                                        height={15}
+                                    />
+                                </a>
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
-        </div>
-    );
+        )
+    )
+        ;
 }
