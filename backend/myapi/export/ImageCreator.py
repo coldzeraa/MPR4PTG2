e52f22f7028de0fad1306151cdb8e8ed4bc21273
Nico Pointner <Nico Pointner@DESKTOP-0FSC8JG>
2024-12-02 15:51:46 +0100

    Add new representation of perimetry result

--- backend/myapi/export/ImageCreator.py
+++ backend/myapi/export/ImageCreator.py
@@ -1,5 +1,11 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 import myapi.db.ResultPerimetryService as PRS
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from scipy.interpolate import griddata
+from io import BytesIO
+
 
 class ImageCreator:
     
@@ -15,8 +21,15 @@ class ImageCreator:
         """
         pointResults = PRS.ResultPerimetryService.getByExID(exID)
         
-        leftImage = ImageCreator.draw_points(ImageCreator.split_array_in_half(pointResults)[0])
-        rightImage = ImageCreator.draw_points(ImageCreator.split_array_in_half(pointResults)[1])
+        pointResultsArr = []
+        matplotlib.use('Agg')
+        for pr in pointResults:
+            pointResultsArr.append([pr.p.x, pr.p.y, 0 if pr.seen else 1])
+                
+        pointsLeftImage, pointsRightImage = ImageCreator.split_array_in_half(pointResultsArr)
+        
+        rightImage = ImageCreator.draw_points(pointsRightImage)
+        leftImage = ImageCreator.draw_points(pointsLeftImage)
         
         combined_width = leftImage.width + rightImage.width
         combined_height = max(leftImage.height, rightImage.height)
@@ -25,7 +38,6 @@ class ImageCreator:
         combined_image.paste(rightImage, (leftImage.width, 0))
         
         return combined_image
-
 
     @staticmethod
     def draw_points(currentPointResult):
@@ -38,27 +50,31 @@ class ImageCreator:
                 Image: Image with drawn points
         """
         
-        width, height = 102, 102
+        pointResultsArr = np.array(currentPointResult)
         
-        image = Image.new('RGB', (width, height), color = 'white')
-        draw = ImageDraw.Draw(image)
+        x, y, seen = pointResultsArr[:, 0], pointResultsArr[:, 1], pointResultsArr[:, 2]
         
-        for pRes in currentPointResult:
-            point = pRes.p
-            seen = pRes.seen
-            pos = ((point.x + 2), (point.y + 2))
+        grid_x, grid_y = np.meshgrid(np.linspace(min(x), max(x), 500),
+                                np.linspace(min(y), max(y), 500))
 
-            if seen:
-                draw.point(pos, fill="green")
-            else:
-                draw.point(pos, fill="red")
-                
-            draw.rectangle([0, 0, width-1, height-1], outline="black")
-            
-        return image
+        grid_z = griddata((x, y), seen, (grid_x, grid_y), method='linear')
 
-        
-        
+        plt.imshow(grid_z, extent=(min(x), max(x), min(y), max(y)), origin='lower',
+                   cmap='Greys', aspect='auto')
+        plt.colorbar(label='Sichtbarkeit (0 = gesehen, 1 = nicht gesehen)')
+        plt.scatter(x, y, color="white", edgecolor='k')
+        plt.xlabel('X-Koordinate')
+        plt.ylabel('Y-Koordinate')
+        plt.tight_layout()
+
+        buffer = BytesIO()
+        plt.savefig(buffer, format='png')
+        plt.close()
+        buffer.seek(0)
+        img = Image.open(buffer).copy()
+        buffer.close()
+        return img
+
     @staticmethod
     def split_array_in_half(array):
         """
@@ -71,9 +87,8 @@ class ImageCreator:
             two arrays first half and second half
         """
         mid = len(array) // 2
-        first_half = array[:mid]
-        second_half = array[mid:]
-        return first_half, second_half
+        return array[:mid], array[mid:]
+ 
         
             
     
--- backend/myapi/views.py
+++ backend/myapi/views.py
@@ -12,6 +12,7 @@ from myapi.point_administrator.PointAdministrator import PointAdministrator
 from myapi.export.EmailSender import EmailSender
 from myapi.export.PdfCreator import PdfCreator
 from myapi.models import Patient
+import random
 import bcrypt
 
 
@@ -92,28 +93,17 @@ def perimetry(request):
 @api_view(['GET'])
 def get_points(request):
     """
-    Retrieves a uniformly distributed list of points from all quadrants and returns them as a JSON response.
+    Retrieves a list of points and returns them as a JSON response.
 
     :param request: The HTTP request object
     :return: A JsonResponse containing a list of points with their x and y coordinates
     """
     
-    # Raw list for points
-    points = []
+    # Get points from database
+    points = PointAdministrator.load_points()
     
-    print("get_points")
-    # Request method
-    if request.method == "GET":
-        # Every quadrant
-        for quadrant in range(1, 5):
-            
-            # Get all points of quadrant
-            currentPoints = PointAdministrator.load_points(quadrant)
-            # Get uniformly distributed indices
-            pointIndices = PointAdministrator.get_uniformed_list(currentPoints)
-
-            # Add points to point list
-            points.extend([currentPoints[i] for i in pointIndices])
+    # Shuffle points
+    random.shuffle(points)
     
     # Send point list to frontend as json
     return JsonResponse({'points': [{'x': p.x, 'y': p.y} for p in points]}, status=200)
